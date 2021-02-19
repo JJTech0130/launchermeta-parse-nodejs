31fdf9e74a7531f79b617ef7ecd0436cd650f277
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2021-02-19 14:56:58 -0500

    some more stuff

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const got = require('got');
 
-(async () => {
-    try {
-        const response = await got('https://launchermeta.mojang.com/mc/game/version_manifest.json');
-        //let body = response.body;
-        //console.log(response.body);
-        version_manifest = JSON.parse(response.body);
-        console.log(version_manifest.versions[0].id);
-        //=> '<!doctype html> ...'
-    } catch (error) {
-        console.log(error);
-        //=> 'Internal server error ...'
-    }
-})();
+async function getManifest(url) {
+    console.log("[getManifest] Downloading " + url);
+    let response = await got(url);
+    let manifest = JSON.parse(response.body);
+    return manifest;
+}
+
+async function main() {
+    let manifest = await getManifest('https://launchermeta.mojang.com/mc/game/version_manifest.json');
+    let versions = manifest.versions;
+
+    versions.forEach(async function(version) {
+        // old_alpha and old_beta versions have weird natives and are not (yet) supported
+        if (!(version.type.includes('old'))) {
+            let vmanifest = await getManifest(version.url);
+            vmanifest.libraries.forEach(async function(lib) {
+                if (lib.name.includes('org.lwjgl.lwjgl:lwjgl-platform:')) {
+                    if (lib.downloads.classifiers['natives-linux'] != undefined) {
+                        console.log(lib.downloads.classifiers['natives-linux'].url);
+                    }
+                    else {
+                        console.log(lib.name + ' does not include \'natives-linux\'');
+                    }
+                }
+            })
+            //console.log(vmanifest.libraries);
+        }
+    });
+}
+
+main();
