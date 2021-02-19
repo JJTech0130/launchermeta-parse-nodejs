008462d13d8ecb44c121739eeaf411cdfcca9039
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2021-02-19 16:48:06 -0500

    more more work

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 const got = require('got');
+const fs = require('fs/promises');
+
+const metahost = 'https://dl.jjtech.dev/';
 
 async function getManifest(url) {
     console.log("[getManifest] Downloading " + url);
+    
     let response = await got(url);
     let manifest = JSON.parse(response.body);
+    
     return manifest;
+}
+
+async function processVersion(version) {
+    if (!(version.type.includes('old'))) {
+        let manifest = await getManifest(version.url);
+
+        for (let lib in manifest.libraries) {
+            
+            if (manifest.libraries[lib].name.includes('org.lwjgl.lwjgl:lwjgl-platform:')) {
+                
+                if (manifest.libraries[lib].downloads.classifiers['natives-linux'] != undefined) {
+                    
+                    let oldurl = manifest.libraries[lib].downloads.classifiers['natives-linux'].url;
+                    let newurl = oldurl.replace('https://libraries.minecraft.net/', metahost);
+                    
+                    console.log('[processVersion: ' + version.id + '] Replacing ' + oldurl + ' with ' + newurl);
+                    
+                    manifest.libraries[lib].downloads.classifiers['natives-linux'].url = newurl;
+
+                }
+            }
+        }
+
+        console.log(manifest);
+    }
 }
 
 async function main() {
     let manifest = await getManifest('https://launchermeta.mojang.com/mc/game/version_manifest.json');
     let versions = manifest.versions;
 
-    versions.forEach(async function(version) {
-        // old_alpha and old_beta versions have weird natives and are not (yet) supported
-        if (!(version.type.includes('old'))) {
-            let vmanifest = await getManifest(version.url);
-            vmanifest.libraries.forEach(async function(lib) {
-                if (lib.name.includes('org.lwjgl.lwjgl:lwjgl-platform:')) {
-                    if (lib.downloads.classifiers['natives-linux'] != undefined) {
-                        console.log(lib.downloads.classifiers['natives-linux'].url);
-                    }
-                    else {
-                        console.log(lib.name + ' does not include \'natives-linux\'');
-                    }
-                }
-            })
-            //console.log(vmanifest.libraries);
-        }
-    });
+    versions.forEach(version => processVersion(version));
 }
 
 main();
