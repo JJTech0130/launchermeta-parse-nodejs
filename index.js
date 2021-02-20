2be3c0da5caff6c5f5d32ac7f5d2a4607f7b78a5
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2021-02-20 16:58:05 -0500

    more

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const got = require('got');
 const fs = require('fs/promises');
+const path = require('path');
+const conf = require('./config.json');
 
-const metahost = 'https://dl.jjtech.dev/';
+const metahost = 'dl.jjtech.dev';
 
 async function getManifest(url) {
     console.log("[getManifest] Downloading " + url);
@@ -12,33 +14,46 @@ async function getManifest(url) {
     return manifest;
 }
 
-async function processVersion(version) {
-    if (!(version.type.includes('old'))) {
-        let manifest = await getManifest(version.url);
-
-        for (let lib in manifest.libraries) {
-            
-            if (manifest.libraries[lib].name.includes('org.lwjgl.lwjgl:lwjgl-platform:')) {
+async function processManifest(manifest) {
+    for (let lib in manifest.libraries) {    
+        if (manifest.libraries[lib].name.includes('org.lwjgl.lwjgl:lwjgl-platform:')) {
+            if (manifest.libraries[lib].downloads.classifiers['natives-linux'] != undefined) {
+                natives_url = new URL(manifest.libraries[lib].downloads.classifiers['natives-linux'].url);
+                natives_url.host = conf.new_host;
                 
-                if (manifest.libraries[lib].downloads.classifiers['natives-linux'] != undefined) {
-                    
-                    let oldurl = manifest.libraries[lib].downloads.classifiers['natives-linux'].url;
-                    let newurl = oldurl.replace('https://libraries.minecraft.net/', metahost);
-                    
-                    console.log('[processVersion: ' + version.id + '] Replacing ' + oldurl + ' with ' + newurl);
-                    
-                    manifest.libraries[lib].downloads.classifiers['natives-linux'].url = newurl;
+                //console.log(natives_url.href);
+                
+                manifest.libraries[lib].downloads.classifiers['natives-linux'].url = natives_url.href;
 
-                }
             }
         }
+    }
+    return manifest;
+}
+async function processVersion(version) {
+    let version_url = new URL(version.url);
+    if (!(version.type.includes('old'))) {
+        let manifest = await getManifest(version_url.href);
+        let new_manifest = await processManifest(manifest);
 
-        console.log(manifest);
+        filename = './out' + version_url.pathname;
+        console.log("Writing manifest to " + filename);
+        fs.mkdir(path.dirname(filename), { recursive: true }, (err) => {
+            if (err) console.log(err);
+            console.log('Created dir');
+        });
+        fs.writeFile(filename,JSON.stringify(new_manifest, null, 4), (err) => {
+            if (err) console.log(err);
+            console.log('wrote file');
+        });
+
+
+        //console.log(manifest);
     }
 }
 
 async function main() {
-    let manifest = await getManifest('https://launchermeta.mojang.com/mc/game/version_manifest.json');
+    let manifest = await getManifest(conf.version_manifest);
     let versions = manifest.versions;
 
     versions.forEach(version => processVersion(version));
